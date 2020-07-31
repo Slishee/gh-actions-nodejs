4eea2949bcf50ccf173c3869f880c41e96dd428a
Rakibul Yeasin <rytotul@gmail.com>
2020-07-31 14:29:10 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ echo "# env";
 sh -c "env";
 
 # Install Packages
-apt install curl
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+wget -qO- https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -y
 apt-get install -y git yarn
