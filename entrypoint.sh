5de51adfbbbc32f65fbc0ed6eafc3e677fbbdeda
Rakibul Yeasin <rytotul@gmail.com>
2020-07-31 14:18:13 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@
 echo "# env";
 sh -c "env";
 
+# Install Packages
+apt install curl
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update -y
+apt-get install -y git yarn
+
 # event.json
 if [ -n "$GITHUB_EVENT_PATH" ]; then
   echo "# event.json";
